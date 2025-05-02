34320560df37da74c7ba8a984f0a2482c4dd8b1c
Jan Czizikow <jan.czizikow@gmail.com>
2025-05-02 13:41:17 +0200

    demo

---
+++ README.md
@@ -0,0 +1,7 @@
+## README
+
+```sh
+yarn # install dependencies
+cp .example.env .env # create environment variables (make sure to add your own host and api key)
+yarn start # run the script that makes update customer session call via SDK
+```
---
+++ index.js
@@ -0,0 +1,69 @@
+require("dotenv").config();
+const crypto = require("crypto");
+
+const TalonOne = require("talon_one");
+
+function sleep(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
+
+(async () => {
+  const defaultClient = TalonOne.ApiClient.instance;
+  const apiPath = process.env.API_BASE_URL || "http://localhost:9000"; // No trailing slash!
+  defaultClient.basePath = apiPath;
+
+  // Configure API key authorization: api_key_v1
+  const api_key_v1 = defaultClient.authentications["api_key_v1"];
+  api_key_v1.apiKey = process.env.API_KEY || "";
+  api_key_v1.apiKeyPrefix = "ApiKey-v1";
+
+  // Integration API example to send a session update
+  const integrationApi = new TalonOne.IntegrationApi();
+
+  // change the default timeout of the api client
+  integrationApi.apiClient.timeout = 1000;
+
+  const abortCallback = (reject) => {
+    console.log("Custom abort callback executed");
+    reject({ error: { code: "CUSTOM_ABORTED", message: "Request aborted" } });
+  };
+
+  try {
+    const id = crypto.randomUUID();
+    console.log(id);
+    const promise = integrationApi.apiClient.callApi(
+      `/v2/customer_sessions/${id}`,
+      "PUT",
+      {}, // path params
+      {}, // query params
+      { Authorization: `ApiKey-v1 ${process.env.API_KEY}` }, // header params
+      {}, // form params
+      {
+        customerSession: {
+          state: "closed",
+          cartItems: [
+            {
+              name: "pizza",
+              sku: "pizza",
+              quantity: 1,
+              price: 100,
+              attributes: {},
+            },
+          ],
+        },
+      }, // body
+      [],
+      ["application/json"],
+      ["application/json"],
+      "Object",
+      null,
+      abortCallback
+    );
+    const res = await promise;
+    console.log(res);
+  } catch (err) {
+    console.log(err);
+  }
+})();
